f4f0f180ec6719767b9f581a6e168afd228fe5c8
gdever <alexey@dodonov.pro>
2020-06-03 12:59:25 +0300

    feature/PdoCrudMock class was added

---
+++ Tests/PdoCrudMock.php
@@ -0,0 +1,81 @@
+<?php
+namespace Mezon\PdoCrud\Tests;
+
+class PdoCrudMock extends \Mezon\PdoCrud\PdoCrud
+{
+
+    public $selectResult = [];
+
+    /**
+     * Getting records
+     *
+     * @param string $fields
+     *            List of fields
+     * @param string $tableNames
+     *            List of tables
+     * @param string $where
+     *            Condition
+     * @param int $from
+     *            First record in query
+     * @param int $limit
+     *            Count of records
+     * @return array List of records
+     */
+    public function select(
+        string $fields,
+        string $tableNames,
+        string $where = '1 = 1',
+        int $from = 0,
+        int $limit = 1000000): array
+    {
+        return $this->selectResult;
+    }
+
+    /**
+     * Counter for update method calls
+     *
+     * @var integer
+     */
+    public $updateWasCalledCounter = 0;
+
+    /**
+     * Updating records
+     *
+     * @param string $tableName
+     *            Table name
+     * @param array $record
+     *            Updating records
+     * @param string $where
+     *            Condition
+     * @param int $limit
+     *            Liti for afffecting records
+     * @return int Count of updated records
+     */
+    public function update(string $tableName, array $record, string $where, int $limit = 10000000)
+    {
+        $this->updateWasCalledCounter ++;
+    }
+
+    /**
+     * Counter for delete method calls
+     *
+     * @var integer
+     */
+    public $deleteWasCalledCounter = 0;
+
+    /**
+     * Deleting records
+     *
+     * @param string $tableName
+     *            Table name
+     * @param string $where
+     *            Condition
+     * @param int $limit
+     *            Liti for afffecting records
+     * @return int Count of deleted records
+     */
+    public function delete($tableName, $where, $limit = 10000000)
+    {
+        $this->deleteWasCalledCounter ++;
+    }
+}
